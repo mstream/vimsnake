13b3b341175e9120bf96b491c2041656d71176c9
mstream <maciej.laciak@gmail.com>
2026-03-14 13:51:06 +0000

    add opencode support

--- flake.nix
+++ flake.nix
@@ -8,13 +8,14 @@
   inputs = {
     flake-utils.url = "github:numtide/flake-utils/main";
     nixpkgs.url = "github:nixos/nixpkgs/master";
-    nixpkgs2511.url = "github:nixos/nixpkgs/release-25.11";
+    nixpkgs2511.url = "github:nixos/nixpkgs/nixos-25.11";
   };
 
   outputs =
     {
       flake-utils,
       nixpkgs,
+      nixpkgs2511,
       ...
     }:
     let
@@ -29,7 +30,7 @@
         pkgs = import nixpkgs {
           inherit system;
         };
-        pkgs2511 = import nixpkgs {
+        pkgs2511 = import nixpkgs2511 {
           inherit system;
           config.allowUnfree = true;
         };
@@ -38,6 +39,7 @@
         devShells.default = pkgs.mkShell {
           packages = [
             pkgs2511.github-copilot-cli
+            pkgs.opencode
             pkgs.spec-kit
           ];
         };
